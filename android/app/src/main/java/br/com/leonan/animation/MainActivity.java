269c3c7a585798db1a5ed40466089cd7eb2bdbd9
Leonan <leonanmattos@gmail.com>
2020-06-02 23:18:47 -0300

    First commit

---
+++ android/app/src/main/java/br/com/leonan/animation/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.leonan.animation;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/home/widgets/stagger_animation.dart
@@ -0,0 +1,38 @@
+import 'package:animation/screens/home/widgets/home_top.dart';
+import 'package:flutter/material.dart';
+
+class StaggerAnimation extends StatelessWidget {
+
+  final AnimationController controller;
+
+  StaggerAnimation({@required this.controller}) :
+    containerGrow = CurvedAnimation(
+      parent: controller,
+      curve: Curves.ease
+    );
+
+  final Animation<double> containerGrow;
+
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        HomeTop(
+          containerGrow: containerGrow,
+        )        
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: AnimatedBuilder(
+          animation: controller,
+          builder: _buildAnimation,
+        ),
+      ),
+    );
+  }
+}
